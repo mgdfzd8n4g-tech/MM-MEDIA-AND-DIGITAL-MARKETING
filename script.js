cc03d58e06c4a1293b7b6e80d50c4bcf7b36c06a
mgdfzd8n4g-tech <mgdfzd8n4g@privaterelay.appleid.com>
2026-06-16 15:30:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    alert("Thank you for contacting MM Media! We will get back to you soon.");
+});
